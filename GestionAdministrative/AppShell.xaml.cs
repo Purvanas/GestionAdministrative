d90e483e941afcf6519a929e79dbb2d7844b2085
Nadir Chettir <nadir.chettir@windfiel.fr>
2025-11-24 11:39:06 +0100

    cration projet MAUI

---
+++ GestionAdministrative/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionAdministrative
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GestionAdministrative/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GestionAdministrative
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
